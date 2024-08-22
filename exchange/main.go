b34f697a6f7878728e670c463f47eab19b476dcc
Kirill Sysoev <k.sysoev@me.com>
2024-08-22 20:22:10 +0800

    Adds basic implementation for exchange server

--- exchange/main.go
+++ exchange/main.go
@@ -1,3 +1,83 @@
 package main
 
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net"
+	"sync"
+
+	"golang.org/x/build/revdial/v2"
+	"tailscale.com/net/socks5"
+)
+
+var (
+	Dialer *revdial.Dialer
+)
+
 // Exchange will be between the client and the server and will be routing and multiplexing client connections to the correct service
+func main() {
+	ctx := context.Background()
+	wg := sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := revConProxyListener(ctx)
+
+		if err != nil {
+			slog.Error(err.Error())
+		}
+	}()
+
+	wg.Wait()
+}
+
+func revConProxyListener(ctx context.Context) error {
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		return err
+	}
+
+	wg := sync.WaitGroup{}
+	for ctx.Err() == nil {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Fatalf("failed to accept: %v", err)
+		}
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			handleConnection(ctx, conn)
+
+		}()
+	}
+
+	return nil
+}
+
+func clientConnProxyListener(ctx context.Context) error {
+	lis, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		return err
+	}
+
+	proxy := socks5.Server{
+		Dialer: dialler,
+	}
+
+	return proxy.Serve(lis)
+}
+
+func dialler(ctx context.Context, _, _ string) (net.Conn, error) {
+	return Dialer.Dial(ctx)
+}
+
+func handleConnection(ctx context.Context, conn net.Conn) {
+	defer conn.Close()
+
+	Dialer = revdial.NewDialer(conn, "")
+
+	<-ctx.Done()
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/ksysoev/oneway
 
 go 1.23.0
 
-require golang.org/x/build v0.0.0-20240820175127-ec1c45197c35 // indirect
+require (
+	github.com/go-json-experiment/json v0.0.0-20231102232822-2e55bd4e08b0 // indirect
+	go4.org/mem v0.0.0-20220726221520-4f986261bf13 // indirect
+	golang.org/x/build v0.0.0-20240821162849-617f3edbcfba // indirect
+	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/sys v0.23.0 // indirect
+	tailscale.com v1.72.0 // indirect
+)
