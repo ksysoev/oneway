1d6fd341e1f6af4a2ea6ab71e532b262964b66a1
Kirill Sysoev <k.sysoev@me.com>
2024-08-24 20:55:03 +0800

    feat: Add example EchoService to handle client requests and provide response

--- example/client/main.go
+++ example/client/main.go
@@ -1,4 +1,34 @@
 package main
 
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+
+	"github.com/ksysoev/oneway/example/api"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
 func main() {
+	ctx := context.Background()
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt, os.Kill)
+	defer cancel()
+
+	conn, err := grpc.NewClient("localhost:9095", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		slog.Error("failed to dial exchange", slog.Any("error", err))
+		return
+	}
+
+	exchangeService := api.NewEchoServiceClient(conn)
+
+	resp, err := exchangeService.Echo(ctx, &api.StringMessage{Value: "Hello, world!"})
+	if err != nil {
+		slog.Error("failed to send message", slog.Any("error", err))
+		return
+	}
+
+	slog.Info("response received", slog.Any("response", resp))
 }
---
+++ example/echoserver/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"net"
+	"os"
+	"os/signal"
+
+	"github.com/ksysoev/oneway/example/api"
+	"google.golang.org/grpc"
+)
+
+type EchoService struct {
+	api.UnimplementedEchoServiceServer
+}
+
+func (s *EchoService) Echo(ctx context.Context, req *api.StringMessage) (*api.StringMessage, error) {
+	slog.Info("Echo", slog.String("message", req.Value))
+
+	return &api.StringMessage{Value: req.Value}, nil
+}
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt, os.Kill)
+	defer cancel()
+
+	grpcServer := grpc.NewServer()
+	api.RegisterEchoServiceServer(grpcServer, &EchoService{})
+
+	lis, err := net.Listen("tcp", ":9095")
+	if err != nil {
+		slog.Error("Failed to listen", slog.Any("error", err))
+		return
+	}
+
+	go func() {
+		<-ctx.Done()
+		grpcServer.GracefulStop()
+	}()
+
+	slog.Info("Server started", slog.String("address", lis.Addr().String()))
+	if err := grpcServer.Serve(lis); err != nil {
+		slog.Error("Failed to serve", slog.Any("error", err))
+	}
+}
