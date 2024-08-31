6335135d199d5a50ba0fb24368e652b9876283d3
Kirill Sysoev <k.sysoev@me.com>
2024-08-31 17:13:21 +0800

    Refactor logic for establishing connections to bridge provider

---
+++ pkg/prov/bridge/bridge.go
@@ -0,0 +1,65 @@
+package bridge
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net"
+
+	"github.com/ksysoev/oneway/api/connection"
+)
+
+type Config struct {
+	Address string
+}
+
+type Connector interface {
+	Connect(ctx context.Context, id uint64) (net.Conn, error)
+}
+
+type Bridge struct {
+	apiClient Connector
+	dialer    net.Dialer
+}
+
+func New(cfg *Config) *Bridge {
+	apiClient := connection.NewClient(cfg.Address)
+
+	return &Bridge{
+		apiClient: apiClient,
+		dialer:    net.Dialer{},
+	}
+}
+
+func (r *Bridge) CreateConnection(ctx context.Context, id uint64, addr string) (src, dest io.ReadWriteCloser, err error) {
+	src, err = r.createBackConnection(ctx, id)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	dest, err = r.createDestConnection(ctx, addr)
+	if err != nil {
+		src.Close()
+		return nil, nil, err
+	}
+
+	return src, dest, nil
+}
+
+func (r *Bridge) createBackConnection(ctx context.Context, id uint64) (io.ReadWriteCloser, error) {
+	conn, err := r.apiClient.Connect(ctx, id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect with for id %d: %w", id, err)
+	}
+
+	return conn, nil
+}
+
+func (r *Bridge) createDestConnection(ctx context.Context, dest string) (io.ReadWriteCloser, error) {
+	connDest, err := r.dialer.DialContext(ctx, "tcp", dest)
+	if err != nil {
+		return nil, fmt.Errorf("failed to dial %s: %w", dest, err)
+	}
+
+	return connDest, nil
+}
