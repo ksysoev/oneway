2c7a49ed2626b47da1bb54d8e7a13d7339264949
Kirill Sysoev <k.sysoev@me.com>
2024-09-05 21:41:07 +0800

    Adds test coverage for bridge provider

---
+++ pkg/prov/bridge/bridge_test.go
@@ -0,0 +1,86 @@
+package bridge
+
+import (
+	"context"
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	cfg := &Config{
+		Address: "example.com:1234",
+	}
+
+	bridge := New(cfg)
+
+	assert.NotNil(t, bridge.apiClient)
+	assert.Equal(t, &net.Dialer{}, bridge.dialer)
+}
+
+func TestBridge_CreateConnection(t *testing.T) {
+	ctx := context.Background()
+	expectedProto := "tcp"
+	expectedAddr := "example.com:1234"
+	expectedId := uint64(1)
+
+	tests := []struct {
+		name        string
+		srcErr      error
+		destErr     error
+		expectedErr error
+	}{
+		{
+			name:        "no errors",
+			srcErr:      nil,
+			destErr:     nil,
+			expectedErr: nil,
+		},
+		{
+			name:        "source error",
+			srcErr:      assert.AnError,
+			destErr:     nil,
+			expectedErr: assert.AnError,
+		},
+		{
+			name:        "destination error",
+			srcErr:      nil,
+			destErr:     assert.AnError,
+			expectedErr: assert.AnError,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			apiClient := NewMockConnector(t)
+			dialer := NewMockContextDialer(t)
+
+			bridgeProv := &Bridge{
+				apiClient: apiClient,
+				dialer:    dialer,
+			}
+
+			srcConn, destConn := net.Pipe()
+
+			apiClient.EXPECT().Connect(ctx, expectedId).Return(srcConn, tt.srcErr)
+
+			if tt.srcErr == nil {
+				dialer.EXPECT().DialContext(ctx, expectedProto, expectedAddr).Return(destConn, tt.destErr)
+			}
+
+			bridge, err := bridgeProv.CreateConnection(context.Background(), uint64(1), expectedAddr)
+
+			if tt.expectedErr != nil {
+				assert.ErrorIs(t, err, tt.expectedErr)
+				assert.Nil(t, bridge)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, bridge)
+			}
+
+			srcConn.Close()
+			destConn.Close()
+		})
+	}
+}
