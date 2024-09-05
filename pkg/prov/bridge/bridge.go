af939f70b76ff382465c7613eef7a9b7f257df7b
Kirill Sysoev <k.sysoev@me.com>
2024-09-05 21:40:53 +0800

    Adds test coverage for bridge provider

--- pkg/prov/bridge/bridge.go
+++ pkg/prov/bridge/bridge.go
@@ -3,7 +3,6 @@ package bridge
 import (
 	"context"
 	"fmt"
-	"io"
 	"net"
 
 	"github.com/ksysoev/oneway/api/revconn"
@@ -18,20 +17,29 @@ type Connector interface {
 	Connect(ctx context.Context, id uint64) (net.Conn, error)
 }
 
-type Bridge struct {
-	apiClient Connector
-	dialer    net.Dialer
+type ContextDialer interface {
+	DialContext(ctx context.Context, network, address string) (net.Conn, error)
 }
 
+type Bridge struct {
+	apiClient Connector
+	dialer    ContextDialer
+}
+
+// New creates a new Bridge instance
+// with the provided configuration
 func New(cfg *Config) *Bridge {
 	apiClient := revconn.NewClient(cfg.Address)
 
 	return &Bridge{
 		apiClient: apiClient,
-		dialer:    net.Dialer{},
+		dialer:    &net.Dialer{},
 	}
 }
 
+// CreateConnection creates a new network bridge connection.
+// It takes a context, connection ID, and address as parameters.
+// It returns a pointer to a network.Bridge and an error.
 func (r *Bridge) CreateConnection(ctx context.Context, id uint64, addr string) (*network.Bridge, error) {
 	src, err := r.createBackConnection(ctx, id)
 	if err != nil {
@@ -47,7 +55,11 @@ func (r *Bridge) CreateConnection(ctx context.Context, id uint64, addr string) (
 	return network.NewBridge(src, dest), nil
 }
 
-func (r *Bridge) createBackConnection(ctx context.Context, id uint64) (io.ReadWriteCloser, error) {
+// createBackConnection creates a connection to the back-end service
+// using the provided connection ID.
+// It takes a context and connection ID as parameters.
+// It returns an io.ReadWriteCloser and an error.
+func (r *Bridge) createBackConnection(ctx context.Context, id uint64) (net.Conn, error) {
 	conn, err := r.apiClient.Connect(ctx, id)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect with for id %d: %w", id, err)
@@ -56,7 +68,11 @@ func (r *Bridge) createBackConnection(ctx context.Context, id uint64) (io.ReadWr
 	return conn, nil
 }
 
-func (r *Bridge) createDestConnection(ctx context.Context, dest string) (io.ReadWriteCloser, error) {
+// createDestConnection creates a connection to the destination service
+// using the provided address.
+// It takes a context and address as parameters.
+// It returns an io.ReadWriteCloser and an error.
+func (r *Bridge) createDestConnection(ctx context.Context, dest string) (net.Conn, error) {
 	connDest, err := r.dialer.DialContext(ctx, "tcp", dest)
 	if err != nil {
 		return nil, fmt.Errorf("failed to dial %s: %w", dest, err)
